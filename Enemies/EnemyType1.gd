140014cc02b9f5299115064c7327145799441ded
RaintL23 <52358003+RaintL23@users.noreply.github.com>
2020-11-09 16:14:00 -0400

    Fix: Enemy 1 Sprite
    Change the EnemyType1 Sprite to a Drone Sprite.

--- Enemies/EnemyType1.gd
+++ Enemies/EnemyType1.gd
@@ -1,11 +1,13 @@
 extends KinematicBody2D
 
 const SPEED = 50
-var animation = "Idle"
+#var animation = "Idle"
 
 onready var stats = $Stats #access to the Stats node
 onready var target = get_parent().get_node("Player")
 
+#func _process(delta):
+#	animation_loop()
 func _ready():
 	print("max healh:", stats.max_health)
 	
@@ -20,8 +22,8 @@ func _physics_process(delta):
 		move_and_collide(velocity * SPEED * delta)
 
 
-func animation_loop():
-	get_node("AnimationPlayer").play(animation)
+#func animation_loop():
+#	get_node("AnimationPlayer").play(animation)
 
 #when detected that "something" entered this area, we're going to delete this enemy. 
 func _on_Hurtbox_area_entered(area):
