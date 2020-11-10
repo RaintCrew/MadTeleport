dc90dfab4805c098a23fe046cffd7c14ebe0e339
RaintL23 <52358003+RaintL23@users.noreply.github.com>
2020-11-10 01:07:16 -0400

    Fix: Moving Folder of the DroneEnemy
    Jejeps well I work on the drone inside the wrong folder... my bad

--- Enemies/EnemyType2Bullet.gd
+++ Enemies/EnemyType2Bullet.gd
@@ -12,9 +12,17 @@ func _physics_process(delta):
 	velocity = velocity.normalized() * speed
 	position = position + velocity
 
+#func _on_EnemyBullet_body_entered(body):
+#	queue_free() # Replace with function body.
+
+
+#func _on_EnemyBullet_area_entered(area):
+#	queue_free() # Replace with function body.
+
+
+func _on_Hitbox_area_entered(area: Area2D) -> void:
+	queue_free()
+
+
 func _on_EnemyBullet_body_entered(body):
-	queue_free() # Replace with function body.
-
-
-func _on_EnemyBullet_area_entered(area):
-	queue_free() # Replace with function body.
+	queue_free()
