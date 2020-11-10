2b8ad5955ef1e1a1178f1bcf270a4cf047a06526
RaintL23 <52358003+RaintL23@users.noreply.github.com>
2020-11-10 02:16:31 -0400

    Feature: Enemy Spawner
    Add a Enemy Spawns in a random location outside the box

--- Enemies/EnemyDrone/EnemyDrone.gd
+++ Enemies/EnemyDrone/EnemyDrone.gd
@@ -36,9 +36,8 @@ func _physics_process(delta: float) -> void:
 			pass
 		CHASE:
 			var player = playerDetectionZone.player
-			if player != null:
-				var direction = (player.global_position - global_position).normalized()
-				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			var direction = (player.global_position - global_position).normalized()
+			velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 			sprite.flip_h = velocity.x < 0
 	velocity = move_and_slide(velocity)
 
