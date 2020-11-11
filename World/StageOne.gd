327a56b1be32ee9b158ac54eeaa3b2533ed1cd84
RaintL23 <52358003+RaintL23@users.noreply.github.com>
2020-11-10 20:42:58 -0400

    Feature: Animated Drone and Knockback

--- World/StageOne.gd
+++ World/StageOne.gd
@@ -4,3 +4,4 @@ func _process(_delta):
 	if Input.is_action_just_pressed("reset"):
 		get_tree().change_scene("res://World/Main.tscn")
 		get_tree().paused = false
+		PlayerStats.health = PlayerStats.max_health
