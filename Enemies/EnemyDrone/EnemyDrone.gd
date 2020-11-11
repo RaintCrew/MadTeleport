b77a8bebdbaf9a6d54a92d7b7be30158040954ce
RaintL23 <52358003+RaintL23@users.noreply.github.com>
2020-11-11 02:23:24 -0400

    Feature: Drone Animated Death

--- Enemies/EnemyDrone/EnemyDrone.gd
+++ Enemies/EnemyDrone/EnemyDrone.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const EnemyDeathEfffect = preload("res://Enemies/EnemyDrone/DroneEnemyDeathEffect.tscn")
+
 export var ACCELERATION = 200
 export var MAX_SPEED = 80
 export var FRICTION = 200
@@ -15,43 +17,20 @@ var knockback = Vector2.ZERO
 
 var state = IDLE
 
-onready var sprite = $Sprite
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 
 func _ready() -> void:
-	#print(stats.max_health)
-	#print(stats.health)
 	pass
 
 func _process(delta: float) -> void:
 	if Global.player:
 		var direction = global_position.direction_to(Global.player.global_position)
 		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
-		#global_position += velocity * MAX_SPEED * delta
 		sprite.flip_h = velocity.x < 0
 		velocity = move_and_slide(velocity)
 
-#func _physics_process(delta: float) -> void:
-#	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
-#	knockback = move_and_slide(knockback)
-#	
-#	match state:
-#		IDLE:
-#			velocity = velocity.move_toward(Vector2.ZERO, 100)
-#			seek_player()
-#		WANDER:
-#			pass
-#		CHASE:
-#			var player = playerDetectionZone.player
-#			var direction = (player.global_position - global_position).normalized()
-#			velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
-#			sprite.flip_h = velocity.x < 0
-#	velocity = move_and_slide(velocity)
-
-#func seek_player():
-#	if playerDetectionZone.can_see_player():
-#		state = CHASE
 
 func _on_Hurtbox_area_entered(area: Area2D) -> void:
 	stats.health -=1
@@ -60,4 +39,8 @@ func _on_Hurtbox_area_entered(area: Area2D) -> void:
 
 func _on_Stats_no_health() -> void:
 	queue_free()
+	var enemyDeathEffect = EnemyDeathEfffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
+	
 
