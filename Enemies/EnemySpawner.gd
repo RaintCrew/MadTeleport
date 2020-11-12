d0094987da917c7609e2d8ef313e7ce2fb11041d
RaintL23 <52358003+RaintL23@users.noreply.github.com>
2020-11-12 15:33:56 -0400

    fix: softcollision and enemy attack
    - when the drone stay in player's hurtbox  still making damage
    - add softcollisions scene and code
    - amount of drones not overlaping

--- Enemies/EnemySpawner.gd
+++ Enemies/EnemySpawner.gd
@@ -2,7 +2,7 @@ extends Position2D
 
 var enemy_drone = preload("res://Enemies/Enemy_drone/EnemyDrone.tscn") # Reference EnemyDrone
 var enemy_tower = preload("res://Enemies/Enemy_tower/EnemyTower.tscn") # Reference EnemyTower
-export(String, "Drone", "Tower") var enemy_type
+export(int, "Drone", "Tower") var enemy_type
 
 export var spawn_delay = 2 				# Spawn speed
 export var enemies_limit = 5;
@@ -12,11 +12,13 @@ var enemies_spawned = 0
 
 func _ready() -> void:
 	get_node("EnemySpawnTimer").wait_time = spawn_delay 			# Set spawn speed to the timer
-	
 	pass
 
 func _on_EnemySpawnTimer_timeout() -> void:
 	if Global.player and enemies_spawned < enemies_limit and spawning: 		# If the player is alive execute below code
 		enemies_spawned += 1
 		var enemy_position = global_position 						# Drone appears in this position
-		Global.instance_node(enemy_drone, enemy_position, self)
+		if enemy_type == 0:
+			Global.instance_node(enemy_drone, enemy_position, self)
+		if enemy_type == 1:
+			Global.instance_node(enemy_tower, enemy_position, self)
---
+++ Hit_Hurtboxes/Hurtbox.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+var invincible = false setget set_invincible
+
+onready var timer = $Timer
+
+signal invincibility_started
+signal invincibility_ended
+
+func set_invincible(value):
+	invincible = value
+	if invincible == true:
+		emit_signal("invincibility_started")
+	else:
+		emit_signal("invincibility_ended")
+
+func start_invincibility(duration):
+	self.invincible = true
+	timer.start()
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_Timer_timeout() -> void:
+	self.invincible = false
+
+func _on_Hurtbox_invincibility_started() -> void:
+	set_deferred("monitorable", false)
+
+func _on_Hurtbox_invincibility_ended() -> void:
+	monitorable = true
