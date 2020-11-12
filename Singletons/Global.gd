49f6bfda5dec1f4db69b21d27c35c1d0860b948d
Carlos Parra <carlos.parra97@gmail.com>
2020-11-12 12:58:52 -0400

    feature: hide mouse cursor

--- Singletons/Global.gd
+++ Singletons/Global.gd
@@ -3,6 +3,9 @@ extends Node
 var node_creation_parent = null
 var player = null
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
 #for now this function works to help spawn enemies
 func instance_node(node, location, parent):
 	var node_instance = node.instance()
