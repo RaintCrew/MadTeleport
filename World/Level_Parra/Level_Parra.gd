464cab7f937e83bb7effd5bdeb8fab57cd42e054
Carlos Parra <carlos.parra97@gmail.com>
2020-11-12 12:24:35 -0400

    feature: create level_parra folder, scene and script

---
+++ World/Level_Parra/Level_Parra.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+onready var currentScene = get_tree().get_current_scene().get_filename()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("reset"):
+		get_tree().change_scene(currentScene)
+		get_tree().paused = false
+		PlayerStats.health = PlayerStats.max_health
+
+	if Input.is_action_just_pressed("esc"):
+		get_tree().quit()
