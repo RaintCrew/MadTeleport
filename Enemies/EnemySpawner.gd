8ff2f47e32c25fa8d367cf5fe79910ce5d32e386
RaintL23 <52358003+RaintL23@users.noreply.github.com>
2020-11-11 22:05:06 -0400

    feature: spawn limit and spawn availabe

--- Enemies/EnemySpawner.gd
+++ Enemies/EnemySpawner.gd
@@ -2,14 +2,18 @@ extends Position2D
 
 var enemy_drone = preload("res://Enemies/Enemy_drone/EnemyDrone.tscn") # Reference EnemyDrone
 
-export var spwan_speed = 2 				# Spawn speed
+export var spwan_delay = 2 				# Spawn speed
+export var enemies_limit = 5;
+export var spawning = true
+
+var enemies_spawned = 0
 
 func _ready() -> void:
-	get_node("EnemySpawnTimer").wait_time = spwan_speed 			# Set spawn speed to the timer
+	get_node("EnemySpawnTimer").wait_time = spwan_delay 			# Set spawn speed to the timer
 	pass
 
 func _on_EnemySpawnTimer_timeout() -> void:
-
-	if Global.player: 									# If the player is alive execute below code
-		var enemy_position = global_position 			# Drone appears in this position
+	if Global.player and enemies_spawned < enemies_limit and spawning: 		# If the player is alive execute below code
+		enemies_spawned += 1
+		var enemy_position = global_position 						# Drone appears in this position
 		Global.instance_node(enemy_drone, enemy_position, self)
