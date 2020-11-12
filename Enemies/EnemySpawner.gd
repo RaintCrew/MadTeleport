3a32f3c18b76962a6a5a0033150d2afbe3f17799
RaintL23 <52358003+RaintL23@users.noreply.github.com>
2020-11-11 22:08:34 -0400

    fix: anyone see this commit!

--- Enemies/EnemySpawner.gd
+++ Enemies/EnemySpawner.gd
@@ -2,14 +2,14 @@ extends Position2D
 
 var enemy_drone = preload("res://Enemies/Enemy_drone/EnemyDrone.tscn") # Reference EnemyDrone
 
-export var spwan_delay = 2 				# Spawn speed
+export var spawn_delay = 2 				# Spawn speed
 export var enemies_limit = 5;
 export var spawning = true
 
 var enemies_spawned = 0
 
 func _ready() -> void:
-	get_node("EnemySpawnTimer").wait_time = spwan_delay 			# Set spawn speed to the timer
+	get_node("EnemySpawnTimer").wait_time = spawn_delay 			# Set spawn speed to the timer
 	pass
 
 func _on_EnemySpawnTimer_timeout() -> void:
