47eaf751a476ea4d6f745d04583be94eb689da88
Enrique <enrix.videos@gmail.com>
2020-11-20 14:13:51 -0500

    feature: add camera shake on saw drone death

--- Enemies/EnemyDeathEffect.gd
+++ Enemies/EnemyDeathEffect.gd
@@ -1,5 +1,6 @@
 extends AnimatedSprite
 
+onready var camera = null
 #
 # Here the animations of death of the enemies are processed
 #
@@ -7,6 +8,7 @@ extends AnimatedSprite
 func _ready() -> void:
 	connect("animation_finished", self, "_on_animation_finished") 	# connect the animation the object (enemy) and execute the function _on_animation_finished
 	play("DeathAnimation") 											# Execute death Animation
+	camera = get_parent().get_parent().get_node("Camera")
 
 
 func _on_animation_finished():
@@ -16,3 +18,4 @@ func _on_animation_finished():
 func _on_DroneEnemyDeathEffect_frame_changed():
 	if frame == 9:
 		$Audio_Explosion.play()
+		camera.activate_shake(3.0,0.4)
