b18338c075d14050711d14a3356fa87b3cdf6923
Enrique <enrix.videos@gmail.com>
2020-11-20 20:23:19 -0500

    feature: add phase 1 of level 1-1.  Polish camera fades
    Codes phase 1 of level 1-1 in the Level1 script. Tweaks things in enemySpawner.gd to better allow the Level script to manipulate it.
    
    Also, changed the timer to restart on the scene to use the "yield" function.

--- World/Level1/Level1.gd
+++ World/Level1/Level1.gd
@@ -6,13 +6,18 @@ var is_restarting = false
 # Player has to kill all enemies of the current phase
 # for the next phase to start. This var keeps track of that
 var num_of_phase_enemies_killed = 0
-var enemies_to_kill_in_phase = [7]
+var enemies_to_kill_in_phase = [8,4]
+
 # The phase of the level (starts at 0)
 # The different waves and setups of enemies
-var phase = 0
+# Its in export var so, in testing, I can skip to test a specific phase
+var phase = 0 setget set_phase
+
+# How many phases are there in this level
+var total_phases = 5
 
 func _ready():
-	set_phase()
+	set_phase(phase)
 	pass
 
 
@@ -20,20 +25,32 @@ func _process(_delta):
 	if Input.is_action_just_pressed("reset"):
 		if not is_restarting:
 			$Camera/AnimationPlayer.play("BlackScreenFadeIn")
-			$Timer_to_Restart_Level1.start() # The scene has to last for some moments for the fadeIn to be seen
 			is_restarting = true
+			yield($Camera/AnimationPlayer, "animation_finished")
+			get_tree().change_scene(currentScene)
+			get_tree().paused = false
+			PlayerStats.health = PlayerStats.max_health
 
 	if Input.is_action_just_pressed("esc"):
 		get_tree().quit()
 
-
-func _on_Timer_to_Restart_Level1_timeout():
-	get_tree().change_scene(currentScene)
-	get_tree().paused = false
-	PlayerStats.health = PlayerStats.max_health
-
+# Called everytime an enemy is killed
 func add_to_enemies_killed():
 	num_of_phase_enemies_killed += 1
+	print("Enemies killed: ",num_of_phase_enemies_killed)
+	if num_of_phase_enemies_killed == enemies_to_kill_in_phase[phase]:
+		set_phase(phase+1)
+		
 
-func set_phase():
-	pass
+# reset(_enemy_type, _spawn_delay, _spawn_offset, _enemies_limit, _spawning, _will_wait_for_death)
+# "Drone" = 1; "Tower" = 2
+func set_phase(value):
+	phase = clamp(value,0,total_phases)
+	print("Phase: ", phase)
+	if phase == 1:
+		$EnemySpawnerUL.reset(1, 1, 0, 2, true, true)
+		$EnemySpawnerDR.reset(1, 1.5, 1, 2, true, true)
+		$EnemySpawnerDL.spawning = false
+		$EnemySpawnerUR.spawning = false
+		$EnemySpawnerCenter.spawning = false
+	
