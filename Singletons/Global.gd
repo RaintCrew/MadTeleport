48d70e110cfac39ff3fbf4f8b41f84a5fbc0c872
Carlos Parra <carlos.parra97@gmail.com>
2020-11-20 22:16:08 -0400

    feature: add fullscreen on game start
    Issue:
    
    No idea why we didn't have this from the beginning.

--- Singletons/Global.gd
+++ Singletons/Global.gd
@@ -2,10 +2,18 @@ extends Node
 
 var node_creation_parent = null
 var player = null
+var screen_size = OS.get_screen_size()
+var window_size = OS.get_window_size()
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	#this centers the game window
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
 
+func _process(delta):
+	if Input.is_action_just_pressed("full_screen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+	
 #for now this function works to help spawn enemies
 func instance_node(node, location, parent):
 	var node_instance = node.instance()
