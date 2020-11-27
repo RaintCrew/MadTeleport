10012ce0b052d202651a6c88c4cb6be2030ce1b0
Miguel Zamora <miguelzamora.23.97@gmail.com>
2020-11-26 23:04:04 -0400

    fix: change main menu bg image.

--- World/Menu/Menu_NewGame_Animation.gd
+++ World/Menu/Menu_NewGame_Animation.gd
@@ -8,3 +8,10 @@ func _ready() -> void:
 func _on_AnimatedSprite_animation_finished() -> void:
 	queue_free()
 	get_tree().change_scene("res://World/Level_Select/Level_Select.tscn")   
+
+
+func _on_AnimatedSprite_frame_changed() -> void:
+	if frame == 21:
+		$TextureRect.hide()
+		$TextureRect2.hide()
+	pass # Replace with function body.
