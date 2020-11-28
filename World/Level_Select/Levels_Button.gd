9001331a610c093d99483971c75a277ffcf8698a
Miguel Zamora <miguelzamora.23.97@gmail.com>
2020-11-27 22:00:40 -0400

    feature: add bg image and music for level 2 and 3

--- World/Level_Select/Levels_Button.gd
+++ World/Level_Select/Levels_Button.gd
@@ -1,19 +1,17 @@
 extends Node2D
 
-var level_tutorial = preload ("res://World/Level_Tutorial/Level_Tutorial.tscn")
-var level_1 = preload ("res://World/Level1/Level1.tscn")
-var level_2 = preload ("res://World/Level2/Level2.tscn")
-var level_3
-
 export(int, "Tutorial", "Level 1", "Level 2", "Level 3") var level_select
 
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 func _on_TextureButton_pressed() -> void:
 	if level_select == 0:
 		get_tree().change_scene("res://World/Level_Tutorial/Level_Tutorial.tscn")
 	if level_select == 1:
 		get_tree().change_scene("res://World/Level1/Level1.tscn")
-	pass # Replace with function body.
+	if level_select == 2:
+		get_tree().change_scene("res://World/Level2/Level2.tscn")
+	if level_select == 3:
+		get_tree().change_scene("res://World/Level3/Level3.tscn")
+	pass
