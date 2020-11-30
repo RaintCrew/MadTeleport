da67adcf046817a32e74a161626b2eff0ee14d13
Miguel Zamora <miguelzamora.23.97@gmail.com>
2020-11-30 19:18:14 -0400

    fix: add back button on Credits

--- World/Level_Credits/LevelCredits.gd
+++ World/Level_Credits/LevelCredits.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	pass
 
+
+
+func _on_ButtonBack_pressed() -> void:
+	get_tree().change_scene("res://World/Menu/MainMenu.tscn")
+	pass
--- World/Level_Select/Level_Select.gd
+++ World/Level_Select/Level_Select.gd
@@ -1,6 +1,7 @@
 extends TextureRect
 
 func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	pass
 
 func _on_BackArrow_pressed() -> void:
