25fad90374cce8bb81c3c468df11e7e3762327ee
Enrique <enrix.videos@gmail.com>
2020-11-29 21:51:58 -0500

    fix: stop 1 bullet from hurting overlapping enemies

--- Enemies/Enemy_drone/EnemyDrone.gd
+++ Enemies/Enemy_drone/EnemyDrone.gd
@@ -38,14 +38,14 @@ func chase_player(delta: float):
 	velocity = move_and_slide(velocity)												# move the drone
 
 func _on_Hurtbox_area_entered(area: Area2D) -> void:
-	stats.health -= area.damage										# Lose a health depending of hit damage
-	
-	velocity = velocity.move_toward(Vector2.ZERO, 300.0)			# Hitting the drone causes it to slow down
-	sprite.modulate = Color(10,10,10,10) 	# Drone sprite turns white
-	$Audio_Hit.play()
-	OS.delay_msec(20)
-	hurt_vfx_timer = DURATION_OF_HURT_VFX	# How long the vfx for being hit lasts
-	area.get_parent().queue_free()									# Delete everything that damages the drone
+	if area != null:
+		stats.health -= area.damage										# Lose a health depending of hit damage
+		area.get_parent().queue_free()
+		velocity = velocity.move_toward(Vector2.ZERO, 300.0)			# Hitting the drone causes it to slow down
+		sprite.modulate = Color(10,10,10,10) 	# Drone sprite turns white
+		$Audio_Hit.play()
+		OS.delay_msec(20)
+		hurt_vfx_timer = DURATION_OF_HURT_VFX	# How long the vfx for being hit last
 
 func _on_Stats_no_health() -> void:
 	emit_signal("enemy_killed")
